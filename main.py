d152cbb8e515928773a77a70b73953a1d301070d
Ivan <162115702+k1an3@users.noreply.github.com>
2024-03-20 11:12:13 +0300

    Update main.py
    created int checks

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 k1 = 1
 k2 = 1.15
 k3 = 1.35
-# functions
 
+m = None
+h = None
+l = None
+k = None
+
+# functions
 def calculateQ():
     Q=k*m*l*lsh//100000
     print("Потраченные калории =", Q)
 
-m = int(input("Введите массу вашего тела (в килограммах): "))
+while m == None:
+    try:
+        m = int(input("Введите массу вашего тела (в килограммах): "))
+    except ValueError:
+        print("Введите число, а не строчку.")
 
-h = int(input("Введите рост вашего тела(в сантиметрах): "))
+while h == None:
+    try:
+        h = int(input("Введите рост вашего тела(в сантиметрах): "))
+    except ValueError:
+        print("Введите число, а не строчку.")
+
 lsh = h//4+37
 print("Интересный факт:", "Длина вашего шага составляет:", lsh)
 
-l = int(input("Введите кол-во пройденных шагов: "))
+while l == None:
+    try:
+        l = int(input("Введите кол-во пройденных шагов: "))
+    except ValueError:
+        print("Введите число, а не строчку.")
+
+
 
 print("Для легкой прогулки: 1")
 print("Для ходьбы с средней скорость: 2")
 print("Для быстрой ходьбы: 3")
-k = int(input("Введите скорость шага: "))
-if k == 2:
-    k = 1.15
-if k == 3:
-    k = 1.35
+while k == None:
+    try:
+        k = int(input("Введите скорость шага: "))
+        if k == 2:
+            k = 1.15
+        if k == 3:
+            k = 1.35
+    except ValueError:
+        print("Введите число, а не строчку.")
 
 calculateQ()
